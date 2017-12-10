b522dc5e75ebe59e13bbd1bd8069ff9f8a86f744
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2017-12-10 18:38:02 +0000

    Added more food

--- planner.js
+++ planner.js
@@ -1,13 +1,26 @@
+function rarity(v, id){
+    if(v=="beef"){
+        document.getElementById(id).style.display = "inline-block"
+    } else {
+        document.getElementById(id).style.display = "none"
+    }
+}
+
 function makeMeat(n){
-    out = "<select id='meatt"+n+"' name='meatt"+n+"'>"
+    out = "<select id='meatt"+n+"' name='meatt"+n+"' onchange=\"rarity(this.value, 'meatr"+n+"')\">"
     out += "<option value='' selected></option>"
-    out += "<option value='turkey'>Turkey</option>"
+//    out += "<option value='turkey'>Turkey</option>"
     out += "<option value='chicken'>Chicken</option>"
     out += "<option value='pork'>Pork (leg or loin)</option>"
     out += "<option value='pork2'>Pork (other)</option>"
     out += "<option value='ham'>Ham</option>"
     out += "<option value='lamb'>Lamb</option>"
     out += "<option value='beef'>Beef</option>"
+    out += "</select>"
+    out += "<select id='meatr"+n+"' name='meatr"+n+"' style='display:none'>"
+    out += "<option value='rare'>rare</option>"
+    out += "<option value='medium' selected>medium</option>"
+    out += "<option value='well-done'>well done</option>"
     out += "</select>"
     out += "&nbsp;Weight:<input id='meatw"+n+"' name='meatw"+n+"' size=1>"
     out += "<select id='meatu"+n+"' name='meatu"+n+"'>"
@@ -27,5 +40,18 @@ function makeVeg(){
 }
 
 
-document.getElementById("meatinputs").innerHTML = makeMeat(1)
-document.getElementById("veginputs").innerHTML = makeVeg()
+meatN = 0
+
+
+function addMeat(){
+    meatN += 1
+    document.getElementById("meatinputs"+meatN).innerHTML += makeMeat(meatN) + "<br />"+"<span id='meatinputs"+(meatN+1)+"'></span>"
+    return False
+}
+function showVeg(){
+    document.getElementById("veginputs").innerHTML = makeVeg()
+}
+
+showVeg()
+addMeat()
+
--- planner.php
+++ planner.php
@@ -2,8 +2,9 @@
 
 $times = Array();
 
-function _add($a, $n){
+function _add($eat, $m, $n){
     global $times;
+    $a = format_time_mins_before($eat, $m);
     if(!isset($times[$a])){$times[$a]=Array();}
     $times[$a][] = $n;
 }
@@ -25,35 +26,60 @@ function format_time_mins_before($t,$m){
     return format_time($t-$m/60);
 }
 
+function temp($n){
+    return $n."&deg;C";
+}
+
 if(isset($_POST['make'])){
 
     print_r($_POST);
+    echo("<br /><br />");
 
     $eat = $_POST['eat-time'];
 
-    echo("<br /><br />");
-
-    _add(format_time($eat),"Eat Christmas dinner");
+    _add($eat,0,"Eat Christmas dinner");
 
     for($i=1;isset($_POST['meatt'.$i]);$i++){
         $t = $_POST['meatt'.$i];
         $w = $_POST['meatw'.$i];
         $u = $_POST['meatu'.$i];
+        $r = $_POST['meatr'.$i];
+        echo($t." ".$w." ".$u." ".$r."<br />");
+        $name = $w.$u." ";
+        if($t=="pork2"){$name.="pork";} else {$name.=$t;}
+
         if($u=="oz"){$w *= 28.35;}
         if($u=="kg"){$w *= 1000;}
-        echo($w."g of ".$t);
-        echo(": ");
         if($t=="pork"){
-            _add(format_time_mins_before($eat,35/450 * $w),"Turn pork down to 190&deg;C");
-            _add(format_time_mins_before($eat,20+35/450 * $w),"Put pork in oven at 245&deg;C");
-            echo("into the oven at ".format_time_mins_before($eat,20+35/450*$w));
+            _add($eat,20 + 35/450 * $w,"Turn ".$name." down to ".temp(190));
+            _add($eat,20 + 20 + 35/450 * $w,"Put ".$name." in oven at ".temp(245));
+            _add($eat,20,"Check the ".$name.". If cooked, take it out and rest it");
         }
         if($t=="pork2"){
-            _add(format_time_mins_before($eat,45/450 * $w),"Turn pork down to 180&deg;C");
-            _add(format_time_mins_before($eat,20+45/450 * $w),"Put pork in oven at 245&deg;C");
-            echo("into the oven at ".format_time_mins_before($eat,20+35/450*$w));
+            _add($eat,20 + 45/450 * $w,"Turn ".$name." down to ".temp(180));
+            _add($eat,20 + 20 + 45/450 * $w,"Put ".$name." in oven at ".temp(245));
+            _add($eat,20,"Check the ".$name.". If cooked, take it out and rest it");
         }
-        echo("<br />");
+        if($t=="chicken"){
+            _add($eat,20 + 20 + 20/450 * $w,"Stuff the ".$name);
+            _add($eat,20 + 20/450 * $w,"Put ".$name." in oven at ".temp(190));
+            _add($eat,20,"Check the ".$name.". If cooked, take it out and rest it");
+        }
+        if($t=="ham"){
+            //_add($eat,20,"Take ".$name."out and rest it");
+            //_add($eat,20,"Take ".$name."out and rest it");
+        }
+        if($t=="lamb"){
+            _add($eat, 30 + 30/450 * $w, "Put ".$name." in the oven at ".temp(230));
+            _add($eat, 30/450 * $w, "Put ".$name." in the oven at ".temp(180));
+        }
+        if($t=="beef"){
+            if($r=="rare"){$cook = 15/450 * $w;}
+            if($r=="medium"){$cook = 30/450 * $w;}
+            if($r=="well-done"){$cook = 45/450 * $w;}
+            _add($eat, 20 + $cook, "Put ".$name." in the oven at ".temp(230));
+            _add($eat, $cook, "Turn ".$name." down to ".temp(190));
+        }
     }
     $k = array_keys($times);
     sort($k);
@@ -63,14 +89,14 @@ if(isset($_POST['make'])){
         echo("<tr><td>".$i."</td><td>".join("<br />",$times[$i])."</td></tr>");
 
     }
-    print_r($times);
 
 } else {
 
     echo("<form method='post'>");
 
     echo("<h2>Meat</h2>");
-    echo("<span id='meatinputs'></span>");
+    echo("<span id='meatinputs1'></span>");
+    echo("<button onclick='return addMeat()' type='button'>+</button>");
 
     echo("<h2>Vegetables</h2>");
     echo("<span id='veginputs'></span>");
